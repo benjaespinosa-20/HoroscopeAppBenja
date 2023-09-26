6a4b06f8399d05cfb6a76e7d71993621db4b337c
benja <benjaespinosa98@gmail.com>
2023-09-25 20:06:39 -0600

    add and configuration Dagger Hilt

---
+++ app/src/main/java/com/example/horoscopeapp/HoroscopeApp.kt
@@ -0,0 +1,7 @@
+package com.example.horoscopeapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class HoroscopeApp:Application()
---
+++ app/src/main/java/com/example/horoscopeapp/ui/luck/LuckViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.horoscopeapp.ui.luck
+
+class LuckViewModel {
+}
